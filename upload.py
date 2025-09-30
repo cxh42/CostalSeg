fcfd5b37b46682b02988a7b79c15f7e77661fcd3
Xingaho Chen <Xhc42@outlook.com>
2025-09-30 16:41:27 -0700

    resetting guide

--- upload.py
+++
@@ -1,9 +0,0 @@
-from huggingface_hub import HfApi, upload_folder
-api = HfApi()
-upload_folder(
-    repo_id="AveMujica/CostalSeg-SJ",
-    repo_type="dataset",
-    folder_path="SegmentModelTraining/SilhouetteJaenette/dataset",
-    path_in_repo="",
-    allow_patterns=["train/*", "valid/*", "test/*", "*.txt"],
-)
